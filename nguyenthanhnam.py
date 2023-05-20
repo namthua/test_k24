60a3544085a10efdce6069c08e3d1c10f0fbef26
Thanhnam <nguyenthanhnamnv996@gmail.com>
2023-05-20 20:00:38 +0700

    finish day1

---
+++ nguyenthanhnam.py
@@ -0,0 +1,11 @@
+name = input("Ten cua ban la gi: ")
+
+#count lenth of string name
+string = name
+length = len(string)
+print(length)
+#reverse string
+string = name
+reversed_string = string[::-1]
+print(reversed_string)
+#
